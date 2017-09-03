733504543f75c3a401aa2330ce3420dac4d5ca1b
Dave <daveyboi@gmail.com>
2017-09-03 13:20:57 +1200

    Changed plan for pacaur, instead of setting facts, just check for packages before building. Also created my own pacaur library with 'sed 's/pacman/pacaur/g' /usr/lib/python2.7/site-packages/ansible/modules/packaging/os/pacman.py > library/pacaur.py' seems to work. Removed duplicate base-devel package install. Fixed typo in desktop firefox package

--- roles/base/files/pacaur.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if pacman -Q pacaur &> /dev/null; then PACAUR="true"; else PACAUR="false"; fi
-if pacman -Q cower &> /dev/null; then COWER="true"; else COWER="false"; fi
-echo {"PACAUR" : "$PACAUR"}
-echo {"COWER" : "$COWER"}
